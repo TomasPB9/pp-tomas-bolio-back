cf99cd05f0d6562361a53e382657ffe0fdbf0875
Tomas Bolio <tomas_9724@hotmail.com>
2021-03-31 23:43:40 -0600

    changing type password

--- src/models/people.js
+++ src/models/people.js
@@ -6,11 +6,11 @@ const PeopleSchema=new Schema({
   name:String,
   email:String,
   phone:Number,
-  password:String,
+  password:{type:String, select:false},
   age:Number,
   gender:String,
   hobbie:String,
-  date:String
+  date:{type:Date, default:Date.now()}
 });
 
 module.exports=mongoose.model('people',PeopleSchema);
